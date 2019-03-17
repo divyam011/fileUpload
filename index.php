b06a1dd530af090f8b5ebec99ade67a565a555c1
divyam011 <ddsingh011@gmail.com>
2019-03-17 21:39:04 +0530

    db woking

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+error_reporting(E_ALL | E_STRICT);
+ini_set('display_errors',1);
+ ?>
 <!DOCTYPE html>
 <html lang="en-US ">
     <head>
@@ -18,10 +22,17 @@
 			<h2>uploaded image</h2>
 			<?php
 				// Include the database configuration file
-				include 'dbConfig.php';
-
-				// Get images from the database
-				$query = $db->query("SELECT * FROM images ORDER BY uploaded_on DESC");
+				$con=mysqli_connect("localhost","root","qwerty","project1");
+// Check connection
+				if ($con)
+				  {
+				  	echo"connection done";
+				  }
+				  else{
+				  die("Connection error: " . mysqli_connect_error());
+				  }
+								// Get images from the database
+				$query = $con->query("SELECT * FROM images ORDER BY uploaded_on DESC");
 
 				if($query->num_rows > 0){
 				    while($row = $query->fetch_assoc()){
--- upload.php
+++ upload.php
@@ -19,7 +19,7 @@ if(isset($_POST["submit"]) & !empty($_FILES["file"]["name"])){
         // Upload file to server
         if(move_uploaded_file($_FILES["file"]["tmp_name"], $targetFilePath)){ 
             // Insert image file name into database
-            $insert = $db->query("INSERT into images (file_name, uploaded_on) VALUES ('".$fileName."', NOW())");
+            $insert = $con->query("INSERT into images (file_name) VALUES ('$fileName')");
             if($insert){
                 $statusMsg = "The file ".$fileName. " has been uploaded successfully.";
             }else{
